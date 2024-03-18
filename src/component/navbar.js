9f5577aa9cfcd52077482f7dbb8199e2fc547dab
nooressam1 <nooressamjx2005@gmail.com>
2024-03-18 19:56:27 +0200

    bottom half of cart with products done

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -197,42 +197,53 @@ const Navbar = (props) => {
             </div>
           </div>
         </div>
-        <div className={`fullscreenmenucart ${open2 ? "active" : "inactive"}`}>
-          <div
-            className="halfscreencart"
-            onClick={() => {
-              setOpen2(!open2);
-              showitems();
-              console.log("pressed");
-            }}
-          ></div>
-          {/* this is for the side bar*/}
-          <div className="cartsidebar">
-            <div className="contentdivcart">
-              <div id="usernamedivcart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="white"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-                <h1>Shopping Cart</h1>
-                <div className="shopchartline"></div>
-              </div>
-              <div className="navbarcontentendcart">
-                <button className="cartendbuttonstyle">
-                  {" "}
-                  No Products in Cart
-                </button>
-              </div>
+      </div>
+
+      <div className={`fullscreenmenucart ${open2 ? "active" : "inactive"}`}>
+        <div
+          className="halfscreencart"
+          onClick={() => {
+            setOpen2(!open2);
+            showitems();
+            console.log("pressed");
+          }}
+        ></div>
+        {/* this is for the side bar*/}
+        <div className="cartsidebar">
+          <div className="contentdivcart">
+            <div id="usernamedivcart">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="white"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                />
+              </svg>
+              <h1>Shopping Cart</h1>
+              <div className="shopchartline"></div>
+            </div>
+          </div>
+          <div className="navbarcontentendcartProducts">
+            <div className="grandtotaldiv">
+              <div className="grandtotalline"></div>
+              <h1 className="totaltextstyle" id="grandtotalplacement">
+                Grand Total
+              </h1>
+              <h1 className="totaltextstyle" id="priceplacement">
+                price
+              </h1>
+              <div className="grandtotalline"></div>
+            </div>
+            <div className="buttondivs">
+              <button id="ContiuneshoppingButton">Contiune Shopping</button>
+              <button id="CheckoutButton">Checkout</button>
             </div>
           </div>
         </div>
@@ -280,9 +291,12 @@ function hideitems() {
 function showitems() {
   if (document.getElementsByClassName("fullscreenmenucart.inactive")) {
     document.getElementById("menuicon").style.display = "inline-block";
-    document.getElementById("accounticon").style.display = "inline-block";
+    if (window.matchMedia("(max-width: 800px)").matches) {
+      document.getElementById("accounticon").style.display = "none";
+    } else {
+      document.getElementById("accounticon").style.display = "inline-block";
+    }
     document.getElementById("shopingcart").style.display = "inline-block";
   }
 }
-
 export default Navbar;
