9f3ff77f0882ecaa2d179951a075a78306e0f516
bigg-khaled <khaled.sharara@gmail.com>
2024-03-23 02:31:04 +0200

    Added comments

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ import {
   Navigate,
 } from "react-router-dom";
 
+// Remove comments after viewing
+// Homepage relocated to src/Pages/Home.js (imported above)
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* Example: <Route path="/shop" element={<Shop />} /> */}
       </Routes>
     </Router>
   );
