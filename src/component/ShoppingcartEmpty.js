f51fc876378fcbd051215bfda1f994c6a83885eb
nooressam1 <nooressamjx2005@gmail.com>
2024-03-29 19:34:36 +0200

    fixed both cart forms and made seperate css files for them

--- src/component/ShoppingcartEmpty.js
+++ src/component/ShoppingcartEmpty.js
@@ -1,44 +1,35 @@
 import React from "react";
 import "../App.css";
+import "../cssComponent/ShoppingcartEmpty.css";
+
 import { useState } from "react";
 
 const ShoppingcartEmpty = () => {
   const [open2, setOpen2] = useState(false);
 
   return (
-    <div className={`fullscreenmenucart ${open2 ? "active" : "inactive"}`}>
-      <div
-        className="halfscreencart"
-        onClick={() => {
-          setOpen2(!open2);
-          showitems();
-          console.log("pressed");
-        }}
-      ></div>
-      {/* this is for the side bar*/}
-      <div className="cartsidebar">
-        <div className="contentdivcart">
-          <div id="usernamedivcart">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="white"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-              />
-            </svg>
-            <h1>Shopping Cart</h1>
-            <div className="shopchartline"></div>
-          </div>
-          <div className="navbarcontentendcart">
-            <h1 className="cartendbuttonstyle"> No Products in Cart</h1>
-          </div>
+    <div className="cartsidebarEmpty">
+      <div className="contentdivcartEmpty">
+        <div id="usernamedivcartEmpty">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="white"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
+          <h1>Shopping Cart</h1>
+          <div className="shopchartlineEmpty"></div>
+        </div>
+        <div className="navbarcontentendcart">
+          <h1 className="cartendbuttonstyle"> No Products in Cart</h1>
         </div>
       </div>
     </div>
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../App.css";
 import react, { useState } from "react";
-import Shoppingitem from "../component/shoppingcartitem";
-import productitem from "../images/productitem.jpg";
+
+import Shoppingcartempty from "../component/ShoppingcartEmpty.js";
 import "../cssComponent/shopingcart.css";
+import "../cssComponent/ShoppingcartEmpty.css";
+import Shoppingcartitem from "../component/shoppingcartitem";
+import Shoppingcart from "./Shoppingcart.js";
+
 const Navbar = (props) => {
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
@@ -201,7 +205,21 @@ const Navbar = (props) => {
           </div>
         </div>
       </div>
+      {/*           this is for the Empty side bar
 
+     <div className={`fullscreenmenucart ${open2 ? "active" : "inactive"}`}>
+        <div
+          className="halfscreencartEmpty"
+          onClick={() => {
+            setOpen2(!open2);
+            showitems();
+            console.log("pressed");
+          }}
+        ></div>
+        <Shoppingcartempty></Shoppingcartempty> 
+ 
+      </div>*/}
+      {/* This is the shopping cart with productts*/}
       <div className={`fullscreenmenucart ${open2 ? "active" : "inactive"}`}>
         <div
           className="halfscreencart"
@@ -211,55 +229,7 @@ const Navbar = (props) => {
             console.log("pressed");
           }}
         ></div>
-        {/* this is for the cart side bar*/}
-        <div className="cartsidebar">
-          <div className="contentdivcart">
-            <div id="usernamedivcart">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="white"
-                class="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                />
-              </svg>
-              <h1>Shopping Cart</h1>
-              <div className="shopchartline"></div>
-            </div>
-          </div>
-          <Shoppingitem
-            image={productitem}
-            productname="Puffer Jacket"
-            price="LE 570.00"
-          ></Shoppingitem>
-          <Shoppingitem
-            image={productitem}
-            productname="Puffer Jacket"
-            price="LE 570.00"
-          ></Shoppingitem>
-          <div className="navbarcontentendcartProducts">
-            <div className="grandtotaldiv">
-              <div className="grandtotalline"></div>
-              <h1 className="totaltextstyle" id="grandtotalplacement">
-                Grand Total
-              </h1>
-              <h1 className="totaltextstyle" id="priceplacement">
-                price
-              </h1>
-              <div className="grandtotalline"></div>
-            </div>
-            <div className="buttondivs">
-              <button id="ContiuneshoppingButton">Contiune Shopping</button>
-              <button id="CheckoutButton">Checkout</button>
-            </div>
-          </div>
-        </div>
+        <Shoppingcart></Shoppingcart>
       </div>
     </div>
   );
